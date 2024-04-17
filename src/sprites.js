65845147a1c4ae910e65cdf72afab3c74d7ea21d
Ruben Nussbaumer <e235522@edu.sbl.ch>
2024-04-17 08:25:31 +0200

    Hintergrund für Level 2 eingefügt.

--- src/sprites.js
+++ src/sprites.js
@@ -107,6 +107,15 @@ export default function loadSprites() {
     lava_gm: { x: 48, y: 32, width: 16, height: 16 },
   })
 
+  k.loadSpriteAtlas("sprites/snow_background.jpg", {
+    snow_background: {
+      x: 0,
+      y: 0,
+      width: 1920,
+      height: 885,
+    },
+  })
+
   // falscher Schlüssel--> kann man nicht einsammeln/ hat keine Funktion
   k.loadSpriteAtlas("sprites/key.png", {
     keyFake: {
